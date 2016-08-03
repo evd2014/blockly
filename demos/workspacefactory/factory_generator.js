32edf064ebf388e7933e8997e163c9e8f25dcb89
Emma Dauterman <evd2014@users.noreply.github.com>
2016-08-03 10:18:51 -0700

    Made changes to refactor parts of generator after adding hidden workspace

--- demos/workspacefactory/factory_generator.js
+++ demos/workspacefactory/factory_generator.js
@@ -17,34 +17,22 @@ FactoryGenerator = function(model) {
   this.model = model;
   // Create hidden workspace to load saved XML to generate toolbox XML.
   var hiddenBlocks = document.createElement('div');
-  hiddenBlocks.id = 'hidden_blocks';
-  document.body.appendChild(hiddenBlocks);
+  // Generate a globally unique ID for the hidden div element to avoid
+  // collisions.
+  var hiddenBlocksId = Blockly.genUid();
+  hiddenBlocks.id = hiddenBlocksId;
   hiddenBlocks.style.display = 'none';
-  this.hiddenWorkspace = Blockly.inject('hidden_blocks');
-};
-
-/**
- * Encodes workspace for a particular category in a XML DOM element. Very
- * similar to workspaceToDom, but doesn't capture IDs. Uses the top-level
- * blocks loaded in hiddenWorkspace.
- *
- * @param {!Element} xmlDom Tree of XML elements to be appended to.
- */
-FactoryGenerator.prototype.categoryWorkspaceToDom = function(xmlDom) {
-  var blocks = this.hiddenWorkspace.getTopBlocks();
-  for (var i = 0, block; block = blocks[i]; i++) {
-    var blockChild = Blockly.Xml.blockToDom(block);
-    blockChild.removeAttribute('id');
-    xmlDom.appendChild(blockChild);
-  }
+  document.body.appendChild(hiddenBlocks);
+  this.hiddenWorkspace = Blockly.inject(hiddenBlocksId);
 };
 
 /**
  * Generates the xml for the toolbox or flyout. If there is only a flyout,
  * only the current blocks are needed, and these are included without
- * a category. If there are categories, then each category is briefly loaded,
- * the user-generated shadow blocks are set as real shadow blocks, and the top
- * blocks are used to generate the xml for the flyout for that category.
+ * a category. If there are categories, then each category is briefly loaded
+ * to the hidden workspace, the user-generated shadow blocks are set as real
+ * shadow blocks, and the top blocks are used to generate the xml for the flyout
+ * for that category.
  *
  * @param {!Blockly.workspace} toolboxWorkspace Toolbox editing workspace where
  * blocks are added by user to be part of the toolbox.
@@ -58,68 +46,96 @@ FactoryGenerator.prototype.generateConfigXml = function(toolboxWorkspace) {
         'id' : 'toolbox',
         'style' : 'display:none'
       });
-  // If no categories, use XML directly from workspace
   if (!this.model.hasToolbox()) {
-    // Load current XML to hidden workspace.
-    var xml = Blockly.Xml.workspaceToDom(toolboxWorkspace);
-    this.hiddenWorkspace.clear();
-    Blockly.Xml.domToWorkspace(xml, this.hiddenWorkspace);
-    // Set user-generated shadow blocks as real shadow blocks.
-    this.setShadowBlocks();
-    // Generate XML from hidden workspace.
-    this.categoryWorkspaceToDom(xmlDom);
-  }
-  else {
+    // Toolbox has no categories. Use XML directly from workspace.
+    this.loadToHiddenWorkspaceAndSave_
+        (Blockly.Xml.workspaceToDom(toolboxWorkspace), xmlDom);
+  } else {
+    // Toolbox has categories.
     // Assert that selected != null
     if (!this.model.getSelected()) {
       throw new Error('Selected is null when the toolbox is empty.');
     }
-    // Capture any changes made by user before generating xml.
+
+    // Capture any changes made by user before generating XML.
     this.model.saveCategoryInList(this.model.getSelected(),
         toolboxWorkspace);
     var xml = this.model.getSelectedXml();
     var toolboxList = this.model.getToolboxList();
-    // Iterate through each category to generate XML for each. Load each
-    // category to make sure that all the blocks that are not top blocks are
-    // also captured as block groups in the flyout.
+
+    // Iterate through each category to generate XML for each using the
+    // hidden workspace. Load each category to the hidden workspace to make sure
+    // that all the blocks that are not top blocks are also captured as block
+    // groups in the flyout.
     for (var i = 0; i < toolboxList.length; i++) {
-      // Create category DOM element.
       var element = toolboxList[i];
       if (element.type == ListElement.SEPARATOR) {
-        var sepElement = goog.dom.createDom('sep');
+        // If the next element is a separator.
+        var nextElement = goog.dom.createDom('sep');
         xmlDom.appendChild(sepElement);
       } else {
-        var categoryElement = goog.dom.createDom('category');
-        categoryElement.setAttribute('name', element.name);
+        // If the next element is a category.
+        var nextElement = goog.dom.createDom('category');
+        nextElement.setAttribute('name', element.name);
         // Add a colour attribute if one exists.
         if (element.color != null) {
-          categoryElement.setAttribute('colour', element.color);
+          nextElement.setAttribute('colour', element.color);
         }
         // Add a custom attribute if one exists.
         if (element.custom != null) {
-          categoryElement.setAttribute('custom', element.custom);
+          nextElement.setAttribute('custom', element.custom);
         }
         // Load that category to hidden workspace, setting user-generated shadow
-        // blocks as real shadow blocks..
-        this.hiddenWorkspace.clear();
-        Blockly.Xml.domToWorkspace(element.xml, this.hiddenWorkspace);
-        this.setShadowBlocks();
-        // Generate XML for that category, append to DOM for all XML.
-        this.categoryWorkspaceToDom(categoryElement);
-        xmlDom.appendChild(categoryElement);
+        // blocks as real shadow blocks.
+        this.loadToHiddenWorkspaceAndSave_(element.xml, nextElement);
       }
+      xmlDom.appendChild(nextElement);
     }
   }
   return xmlDom;
  };
 
 /**
+ * Load the given XML to the hidden workspace, set any user-generated shadow
+ * blocks to be actual shadow blocks, then append the XML from the workspace
+ * to the DOM element passed in.
+ * @private
+ *
+ * @param {!Element} xml The XML to be loaded to the hidden workspace.
+ * @param {!Element} dom The DOM element to append the generated XML to.
+ */
+FactoryGenerator.prototype.loadToHiddenWorkspaceAndSave_ = function(xml, dom) {
+  this.hiddenWorkspace.clear();
+  Blockly.Xml.domToWorkspace(xml, this.hiddenWorkspace);
+  this.setShadowBlocksInHiddenWorkspace_();
+  this.appendHiddenWorkspaceToDom_(dom);
+}
+
+ /**
+ * Encodes blocks in the hidden workspace in a XML DOM element. Very
+ * similar to workspaceToDom, but doesn't capture IDs. Uses the top-level
+ * blocks loaded in hiddenWorkspace.
+ * @private
+ *
+ * @param {!Element} xmlDom Tree of XML elements to be appended to.
+ */
+FactoryGenerator.prototype.appendHiddenWorkspaceToDom_ = function(xmlDom) {
+  var blocks = this.hiddenWorkspace.getTopBlocks();
+  for (var i = 0, block; block = blocks[i]; i++) {
+    var blockChild = Blockly.Xml.blockToDom(block);
+    blockChild.removeAttribute('id');
+    xmlDom.appendChild(blockChild);
+  }
+};
+
+/**
  * Sets the user-generated shadow blocks loaded into hiddenWorkspace to be
  * actual shadow blocks. This is done so that blockToDom records them as
  * shadow blocks instead of regular blocks.
+ * @private
  *
  */
-FactoryGenerator.prototype.setShadowBlocks = function() {
+FactoryGenerator.prototype.setShadowBlocksInHiddenWorkspace = function() {
   var blocks = this.hiddenWorkspace.getAllBlocks();
   for (var i = 0; i < blocks.length; i++) {
     if (this.model.isShadowBlock(blocks[i].id)) {
